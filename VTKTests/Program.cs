ef98650ab6f508cd0df8340a07ff52cb3d9a535f
Aesir MacGregor <aesirmacgregor@outlook.de>
2018-03-28 14:19:23 +0200

    Implementing BorderScan

--- VTKTests/Program.cs
+++ VTKTests/Program.cs
@@ -50,9 +50,10 @@ namespace SciVis
             try
             {
                 //AnalyseMat(Data);
-                AnalyseMaterials(Data);
+                //AnalyseMaterials(Data);
                 //Analyse(Data);
                 //AnalyseLayer(Data);
+                //BorderScan(Data);
             }
             catch (Exception ex)
             {
@@ -65,6 +66,7 @@ namespace SciVis
             if (Debugger.IsAttached) Debugger.Break();
             //Console.ReadKey();
         }
+
         public static vtkImageData ReadInData(string FileName)
         {
             vtkXMLImageDataReader Reader = new vtkXMLImageDataReader();
@@ -102,6 +104,38 @@ namespace SciVis
                 }
             }
             Display("Preasurest Point at {0} is {1}", BiggestValue.Index, BiggestValue.Value);
+        }
+
+        // Erfasst alle Grenzpunkte eines bestimmten Materials und stellt sie in einer Bordermap fest
+        private static void BorderScan(vtkImageData FileContent)
+        {
+            MeteorData Data = new MeteorData(FileContent.GetPointData());
+            List<long> BorderMap = new List<long>();
+
+            //legt Material, nachdem gesucht wird, fest
+            float Material = 3;
+            bool BodyFound = false;
+
+            for(int x = 0; x <= 300; x++)
+            {
+                for (int y = 0; y <= 300; y++)
+                {
+                    for (int z = 0; z <= 300; z++)
+                    {
+                        var (Index, Value) = Data.mat.GetPoint(x, y, z);
+                        if (Material == Value && BodyFound == false)
+                        {
+                            BorderMap.Add(Index);
+                            BodyFound = true;
+                        }
+                        else if(Material != Value && BodyFound == true)
+                        {
+                            BorderMap.Add(Index);
+                            BodyFound = false;
+                        }
+                    }
+                }
+            }
         }
 
         public static void AnalyseLayer(vtkImageData FileContent)
