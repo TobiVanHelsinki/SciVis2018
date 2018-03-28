ed675b7e4ae85363ecbf9a114b95eb5d50f04657
Tobias Pabst <TobivanHelsinki@live.de>
2018-03-28 12:46:43 +0200

    added doc

--- VTKTests/Model/MeteorData.cs
+++ VTKTests/Model/MeteorData.cs
@@ -9,22 +9,69 @@ namespace SciVis.Model
 {
     public class MeteorData
     {
+        /// <summary>
+        /// no clue what this is
+        /// </summary>
         public PointDataList<int> vtkGhostType;
+        /// <summary>
+        /// no clue what this is
+        /// </summary>
         public PointDataList<int> vtkValidPointMask;
 
+        /// <summary>
+        /// rho density in grams per cubic centimeter. (g/cm3)
+        /// </summary>
         public PointDataList<Single> rho;
+        /// <summary>
+        /// prs pressure in microbars (µbar)
+        /// </summary>
         public PointDataList<Single> prs;
+        /// <summary>
+        /// tev temperature in electronvolt (eV)
+        /// </summary>
         public PointDataList<Single> tev;
+        /// <summary>
+        /// xdt x component vectors in centimeters per second (cm/sec)
+        /// </summary>
         public PointDataList<Single> xdt;
+        /// <summary>
+        /// ydt y component vectors in centimeters per second (cm/sec)
+        /// </summary>
         public PointDataList<Single> ydt;
+        /// <summary>
+        /// zdt z component vectors in centimeters per second (cm/sec)
+        /// </summary>
         public PointDataList<Single> zdt;
+        /// <summary>
+        /// snd sound speed in centimeters per second (cm/sec)
+        /// </summary>
         public PointDataList<Single> snd;
+        /// <summary>
+        /// grd AMR grid reﬁnement level
+        /// </summary>
         public PointDataList<Single> grd;
+        /// <summary>
+        /// mat material number id
+        /// </summary>
         public PointDataList<Single> mat;
+        /// <summary>
+        /// v02 volume fraction water
+        /// </summary>
         public PointDataList<Single> v02;
+        /// <summary>
+        /// v03 volume fraction of asteroid
+        /// </summary>
         public PointDataList<Single> v03;
 
+        /// <summary>
+        /// the raw data, this class uses
+        /// </summary>
         public vtkPointData PointData;
+
+        /// <summary>
+        /// create a new wrapper vor meteor point data
+        /// </summary>
+        /// <param name="iPointData"></param>
         public MeteorData(vtkPointData iPointData)
         {
             PointData = iPointData;
